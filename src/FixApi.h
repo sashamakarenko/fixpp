94a8151dfc48c2113508f5f4bcfc5a1418944e65
alex <alex@home.fr>
2021-12-25 06:33:13 +0100

    inline parseUInt and parseDouble

--- src/FixApi.h
+++ src/FixApi.h
@@ -62,6 +62,29 @@ constexpr static const unsigned uintPow10[] =
     100000000U,
     1000000000U
 };
+
+constexpr static const double div10Pow[] =
+{
+    1,
+    0.1,
+    0.01,
+    0.001,
+    0.0001,
+    0.00001,
+    0.000001,
+    0.0000001,
+    0.00000001,
+    0.000000001,
+    0.0000000001,
+    0.00000000001
+};
+
+// valid for FIX only
+constexpr bool isNotDecDigit( char c )
+{
+    return c < '0';
+}
+
 }
 
 // used to scan integers of type T
@@ -107,9 +130,99 @@ constexpr uint64_t dec_zeros<uint64_t,0UL>()
     return 0UL;
 }
 
-unsigned parseUInt( const char * ptr, unsigned & len );
+template< typename T = unsigned >
+inline T parseUInt( const char * ptr, unsigned & len )
+{
+    if( isNotDecDigit( ptr[0] ) )
+    {
+        return 0;
+    }
+    if( isNotDecDigit( ptr[1] ) )
+    {
+        len += 1;
+        return T( ptr[0] ) - dec_zeros<T,T(1)>();
+    }
+    if( isNotDecDigit( ptr[2] ) )
+    {
+        len += 2;
+        return T( ptr[0] ) * 10 + T( ptr[1] ) - dec_zeros<T,T(2)>();
+    }
+    if( isNotDecDigit( ptr[3] ) )
+    {
+        len += 3;
+        return T( ptr[0] ) * 100 + T( ptr[1] ) * 10 + T( ptr[2] ) - dec_zeros<T,T(3)>();
+    }
+    if( isNotDecDigit( ptr[4] ) )
+    {
+        len += 4;
+        return T( ptr[0] ) * 1000 + 
+               T( ptr[1] ) * 100 + 
+               T( ptr[2] ) * 10 + 
+               T( ptr[3] ) - dec_zeros<T,T(4)>();
+    }
+    if( isNotDecDigit( ptr[5] ) )
+    {
+        len += 5;
+        return T( ptr[0] ) * 10000 + 
+               T( ptr[1] ) * 1000 + 
+               T( ptr[2] ) * 100 + 
+               T( ptr[3] ) * 10 + 
+               T( ptr[4] ) - dec_zeros<T,T(5)>();
+    }
+    if( isNotDecDigit( ptr[6] ) )
+    {
+        len += 6;
+        return T( ptr[0] ) * 100000 + 
+               T( ptr[1] ) * 10000 + 
+               T( ptr[2] ) * 1000 + 
+               T( ptr[3] ) * 100 + 
+               T( ptr[4] ) * 10 + 
+               T( ptr[5] ) - dec_zeros<T,T(6)>();
+    }
+    if( isNotDecDigit( ptr[7] ) )
+    {
+        len += 7;
+        return T( ptr[0] ) * 1000000 + 
+               T( ptr[1] ) * 100000 + 
+               T( ptr[2] ) * 10000 + 
+               T( ptr[3] ) * 1000 + 
+               T( ptr[4] ) * 100 + 
+               T( ptr[5] ) * 10 +
+               T( ptr[6] ) - dec_zeros<T,T(7)>();
+    }
+    T tmp =    T( ptr[0] ) * 10000000 + 
+               T( ptr[1] ) * 1000000 + 
+               T( ptr[2] ) * 100000 + 
+               T( ptr[3] ) * 10000 + 
+               T( ptr[4] ) * 1000 + 
+               T( ptr[5] ) * 100 +
+               T( ptr[6] ) * 10 +
+               T( ptr[7] ) - dec_zeros<T,T(8)>();
+    if( isNotDecDigit( ptr[8] ) )
+    {
+        len += 8;
+        return tmp;
+    }
 
-double parseDouble( const char * ptr );
+    unsigned tmplen = 0;
+    T next = parseUInt<T>( ptr + 8, tmplen );
+    len += tmplen + 8;
+    return tmp * uintPow10[ tmplen ] + next;
+}
+
+inline double parseDouble( const char * ptr )
+{
+    unsigned intlen = 0;
+    unsigned integer = parseUInt( ptr, intlen );
+    double mantissa = 0.0;
+    if( ptr[intlen] == '.' )
+    {
+        unsigned mantissaLength = 0;
+        mantissa = parseUInt( ptr + intlen + 1, mantissaLength );
+        mantissa *= div10Pow[ mantissaLength ];
+    }
+    return (double)integer + mantissa;
+}
 
 template< typename T = unsigned >
 inline const char * parseYYYYMMDD( const char * ptr, T & year, T & month, T & day )
